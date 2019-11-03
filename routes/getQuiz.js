042c6c515bb5d3022c6c31db362919f4690864d7
AlexJ2005 <alexanderjernstrom@gmail.com>
2019-11-03 19:28:20 +0100

    chaanging the queriying

--- routes/getQuiz.js
+++ routes/getQuiz.js
@@ -4,14 +4,14 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     let {name} = req.query;
-
+    
     
     Quiz.find((err, quiz) => {
         if(err || !quiz){
             return res.status(404).json({error: "No quizes were found"})
         }    
         const searchedQuiz = quiz.filter((quiz) => {return quiz.name.includes(name)})
-        if(name == undefined){
+        if(name == undefined || name==''){
         return res.send(quiz)
         } else if(name){
             res.send(searchedQuiz)
