017e5ffa37e4595e20f074bc20041cde98b02d49
AlexJ2005 <alexanderjernstrom@gmail.com>
2019-11-28 15:50:16 +0100

    fixed small bug

--- routes/createQuiz.js
+++ routes/createQuiz.js
@@ -12,7 +12,8 @@ router.post("/", async (req, res) => {
     const newQuiz = new Quiz({
       name: req.body.name,
       questions: req.body.questions,
-      createdBy: user.name
+      createdBy: user.name,
+      date: Date.now
     });
 
     newQuiz
