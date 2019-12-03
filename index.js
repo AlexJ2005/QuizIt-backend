5c0edaa1d4ab17a5d8dfd08de056d23b2dbf91ca
AlexJ2005 <alexanderjernstrom@gmail.com>
2019-12-03 07:57:53 +0100

    ip tracking

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const createUser = require("./routes/createUser");
 const getIndividualUser = require("./routes/getIndividualUser");
 const ipMiddleware = function(req, res, next) {
   const clientIp = requestIp.getClientIp(req);
-  console.log(clientIp + Date.now());
+  console.log(clientIp + Date(Date.now()));
   next();
 };
 
